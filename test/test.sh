cd80f19c00447c4f80b8773b01a87c253e95e0bb
Lars Lien <Lars@familienlien.com>
2026-03-03 20:11:05 +0100

    Start of test script

---
+++ test/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# To make runnable in Linux run: chmod +x myscript.sh
+# Goal: Make a test script that runs up to 3 elevators allowing us to test everything locally. 
+
+echo "Kjører test!"
+
+
+gnome-terminal -- ./server/SimElevatorServer

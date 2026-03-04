d9f4cf1da72766989315070c8044838515587bf8
Lars Lien <Lars@familienlien.com>
2026-03-04 12:27:53 +0100

    More test tings

--- test/test.sh
+++ test/test.sh
@@ -4,5 +4,28 @@
 
 echo "Kjører test!"
 
+cleanup() {
+    echo "Test stopped. Killing gnome-termoinals..."
 
-gnome-terminal -- ./server/SimElevatorServer
+    pkill gnome-terminal
+    # rm -rf build
+}
+
+trap cleanup EXIT INT TERM
+
+# gnome-terminal -- bash -c "./server/SimElevatorServer; exec bash" &
+
+gnome-terminal -- bash -c "echo "Heis-1"; ./SimElevatorServer --port=15657; exec bash"
+sleep 0.2
+gnome-terminal -- bash -c "cd ../driver-go; go run main.go; exec bash"
+
+
+
+# gnome-terminal -- bash -c "./SimElevatorServer --port=15658; exec bash"
+
+# gnome-terminal -- bash -c "./SimElevatorServer --port=15659; exec bash"
+
+#What untill killed. 
+while true; do
+    sleep 1
+done

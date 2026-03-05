54c5ce8074ab637c6ab50507e30789b5c588c16f
Lars Lien <Lars@familienlien.com>
2026-03-05 16:39:03 +0100

    Mer network

--- src/main.go
+++ src/main.go
@@ -9,10 +9,12 @@ import (
 // og kanalene de snakker sammen på
 
 func main(){
+	//VIKTIG: Hvis man trenger å endre i denne fila, feks for teste andre deler av systemt så comment ut det som er her. 
+	
 	// Our id can be anything. Here we pass it on the command line, using
 	//  `go run main.go -id=our_id`
 	var id string
-	flag.StringVar(&id, "id", "", "id of this peer")
+	flag.StringVar(&id, "id", "", "id of this elevator")
 	flag.Parse()
 
 	
--- src/network/slaves.go
+++ src/network/slaves.go
@@ -1,32 +1,102 @@
 package network
 
+//Alot of andoutcode from "peers" but modifies to master/slave architecture.
+
 import (
-	"Network-go/network/bcast"
 	"Network-go/network/conn"
 	"fmt"
 	"net"
+	"sort"
 	"time"
 )
 
-//The goal of this module is to see how many slaves the master have.
+// The goal of this module is to see how many slaves the master have.
 // This is done by sending heartbeats,
 // listen to reponses snd then manage a list of the active slaves.
 
 //Input: Only has to be started/stopped when promoted/revoked to/from master.
 //Out: Can be pulled to return the current list of slaves.
 
+//Ting som skal være i reeelection og ikke her:
+// Check for double master.
+// Check for no master.
+
 type HelloMsg struct {
 	Message string
 	Iter    int
 }
 
-const heartBeatInterval = 15 * time.Millisecond
+type PeerUpdate struct {
+	Peers []string
+	New   string
+	Lost  []string
+}
 
-func MasterActions(port int, id string, isMaster <-chan bool) {
+const heartBeatInterval = 15 * time.Millisecond
+const timeout = 500 * time.Millisecond
+
+func MasterActions(port int, id string, isMaster <-chan bool, peerUpdate chan PeerUpdate) {
 
 	conn := conn.DialBroadcastUDP(port)
 	addr, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("255.255.255.255:%d", port))
 
+	go SendHeartbeats(conn, addr, isMaster)
+	//go ReceveFromSlaves(conn, peerUpdate)
+
+
+	
+}
+
+func ReceveFromSlaves(conn net.PacketConn, peerUpdateCh chan PeerUpdate){
+	var buf [1024]byte
+	var p PeerUpdate
+	lastSeen := make(map[string]time.Time)
+
+	for {
+		updated := false
+
+		conn.SetReadDeadline(time.Now().Add(heartBeatInterval))
+		n, _, _ := conn.ReadFrom(buf[0:])
+
+		id := string(buf[:n])
+
+		// Adding new connection
+		p.New = ""
+		if id != "" {
+			if _, idExists := lastSeen[id]; !idExists {
+				p.New = id
+				updated = true
+			}
+
+			lastSeen[id] = time.Now()
+		}
+
+		// Removing dead connection
+		p.Lost = make([]string, 0)
+		for k, v := range lastSeen {
+			if time.Now().Sub(v) > timeout {
+				updated = true
+				p.Lost = append(p.Lost, k)
+				delete(lastSeen, k)
+			}
+		}
+
+		// Sending update
+		if updated {
+			p.Peers = make([]string, 0, len(lastSeen))
+
+			for k, _ := range lastSeen {
+				p.Peers = append(p.Peers, k)
+			}
+
+			sort.Strings(p.Peers)
+			sort.Strings(p.Lost)
+			peerUpdateCh <- p
+		}
+	}
+}
+
+func SendHeartbeats(conn net.PacketConn, addr *net.UDPAddr, isMaster <-chan bool){
 	enable := true
 	for {
 		select {
@@ -34,20 +104,29 @@ func MasterActions(port int, id string, isMaster <-chan bool) {
 		case <-time.After(heartBeatInterval):
 		}
 		if enable {
-			conn.WriteTo([]byte(id), addr)
+			conn.WriteTo([]byte("master"), addr)
+		}
+	}
+
+}
+
+func SlaveActions(id string, conn net.PacketConn){
+
+	var buf [1024]byte
+	
+	for {
+		n, _, _ := conn.ReadFrom(buf[0:])
+
+		id := string(buf[:n])
+
+		if id != ""{
+			fmt.Println("Recived from id: ", id)
+			// conn.WriteTo([]byte(id + ":ack"), addr)
 		}
 	}
 	
-}
 
-func SlaveActions(id string, done chan bool){
 	
-	reciveBrodcast := make(chan HelloMsg)
+	// done <- true
 
-	go bcast.Receiver(16569, reciveBrodcast)
-	for {
-		a := <-reciveBrodcast
-		fmt.Printf("Received: %#v\n", a)
-	}
-	done <- true
 }

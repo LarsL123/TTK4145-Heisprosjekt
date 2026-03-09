9335fa889bf7ac9600cd1c17503fa63a00fdee1d
Lars Lien <Lars@familienlien.com>
2026-03-09 10:33:01 +0100

    Speech

--- src/donaldtrump/donaldtrump.go
+++ src/donaldtrump/donaldtrump.go
@@ -1,8 +1,9 @@
 package donaldtrump
 
-
 /*
 
-
+ Im gonne create the greates eleavtor youll ever see. Its gonne go to 1000 floors, yeah it can go to the moon.
+ It will be the greatest and fastest elevator in the history of elevators.
+ And we gonne make china pay for it.
 
 */

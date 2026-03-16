5e64a5ca2bc2597ecfbf9f17bc4e54629b411ed7
Lars Lien <Lars@familienlien.com>
2026-03-16 18:12:49 +0100

    Tester noe nettwork

--- src/network/network.go
+++ src/network/network.go
@@ -50,84 +50,63 @@ func (r *GenericSender[A, B]) SendAsyncWithAck(msg A) {
 	}()
 }
 
-//Sends OrderAndStateUpdates Async to the server. Sending a new update will cancel the previous one if its still trying to send.
-// func (r *AssignmentSender) UpdateAsync(msg AssignmentsAndOrders) {
+
+// func (r *GenericSender[A, B]) SendAsyncWithAck(msg A) {
 //     r.mu.Lock()
-//      // cancel previous send if exists
 //     if r.cancelLast != nil {
-//         r.cancelLast()
+//         r.cancelLast() // cancel previous send
 //     }
+
 //     ctx, cancel := context.WithCancel(context.Background())
 //     r.cancelLast = cancel
-//     r.lastUpdateNr = msg.UpdateNr
+//     r.lastUpdateNr = msg.GetUpdateNr()
 //     r.mu.Unlock()
 
+//     ackCh := make(chan B, 1) // per-message ACK channel
+
+//     // Optional: register this ackCh with a dispatcher if AckIn is shared
 //     go func() {
+//         defer close(ackCh)
+
 //         retryTicker := time.NewTicker(config.Cfg.AckRetryRate)
 //         defer retryTicker.Stop()
 
 //         timeoutTimer := time.NewTimer(config.Cfg.AckTimeout)
 //         defer timeoutTimer.Stop()
 
-//         r.SendCh <- msg
-
-//         for {
+//         send := func() bool {
 //             select {
+//             case r.SendCh <- msg:
+//                 return true
 //             case <-ctx.Done():
-//                 // new message canceled this send
-//                 return
-//             case <-retryTicker.C:
-//                 r.SendCh <- msg
-//             case ack := <-r.AckIn:
-//                 if ack.UpdateNr == msg.UpdateNr {
-//                     r.AckResults <- AckResult{UpdateNr: ack.UpdateNr, Err: nil}
-//                     return
-//                 }
-//                 // ignore ACKs for older messages ord other units
-//             case <-timeoutTimer.C:
-//                 r.AckResults <- AckResult{UpdateNr: msg.UpdateNr, Err: fmt.Errorf("timeout waiting for ack assignmentSender")}
-//                 return
+//                 return false
 //             }
 //         }
-//     }()
-// }
 
-//Sends OrderAndStateUpdates Async to the server. Seding a new update will cancel the previous one if its still trying to send.
-// func (r *OrderSender) UpdateAsync(msg OrdersAndStateUpdate) {
-//     r.mu.Lock()
-//      // cancel previous send if exists
-//     if r.cancelLast != nil {
-//         r.cancelLast()
-//     }
-//     ctx, cancel := context.WithCancel(context.Background())
-//     r.cancelLast = cancel
-//     r.lastUpdateNr = msg.UpdateNr
-//     r.mu.Unlock()
-
-//     go func() {
-//         retryTicker := time.NewTicker(config.Cfg.AckRetryRate)
-//         defer retryTicker.Stop()
-
-//         timeoutTimer := time.NewTimer(config.Cfg.AckTimeout)
-//         defer timeoutTimer.Stop()
-
-//         r.SendCh <- msg
+//         if !send() {
+//             return
+//         }
 
 //         for {
 //             select {
 //             case <-ctx.Done():
-//                 // new message canceled this send
 //                 return
 //             case <-retryTicker.C:
-//                 r.SendCh <- msg
+//                 send()
 //             case ack := <-r.AckIn:
-//                 if ack.UpdateNr == msg.UpdateNr {
-//                     r.AckResults <- AckResult{UpdateNr: ack.UpdateNr, Err: nil}
+//                 if ack.GetUpdateNr() == msg.GetUpdateNr() {
+//                     select {
+//                     case r.AckResults <- AckResult{ack.GetUpdateNr(), nil}:
+//                     case <-ctx.Done():
+//                     }
 //                     return
 //                 }
-//                 // ignore ACKs for older messages ord other units
+//                 // ignore other ACKs
 //             case <-timeoutTimer.C:
-//                 r.AckResults <- AckResult{UpdateNr: msg.UpdateNr, Err: fmt.Errorf("timeout waiting for ack orderSender")}
+//                 select {
+//                 case r.AckResults <- AckResult{msg.GetUpdateNr(), fmt.Errorf("timeout waiting for ack")}:
+//                 case <-ctx.Done():
+//                 }
 //                 return
 //             }
 //         }

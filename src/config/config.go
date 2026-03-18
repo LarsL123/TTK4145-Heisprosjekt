3cde2b063bc8c85c4f6a4a19b8d41133f1383ddf
uyeennh <uyennh@stud.ntnu.no>
2026-03-18 16:11:23 +0100

    Changed maxelevatorsuspendtime

--- src/config/config.go
+++ src/config/config.go
@@ -47,7 +47,7 @@ var defaultValues = Config{
 	ElevatorUpdateRate: 200 * time.Millisecond,
 
 	MaxOrderSuspendTime:    20 * time.Second,
-	MaxElevatorSuspendTime: 15 * time.Second,
+	MaxElevatorSuspendTime: 5 * time.Second,
 
 	NewBackupTimeoutTime: 500 * time.Millisecond, //Needs to be bigger than heartbeatinterval
 	NewMasterTimeoutTime: 500 * time.Millisecond,
